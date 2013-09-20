33aed3a7da55c2a70ebe649335c6b097ddf47245
Tiffany Green <tiffanydgreen@gmail.com>
2013-09-20 11:30:33 -0500

    add google plus gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 end
