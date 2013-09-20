096b607f7d635ceb0979797f412daa6812adac4a
Steve Faulkner <southpolesteve@gmail.com>
2013-09-20 11:39:20 -0500

    Add finders for each identity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :identities
+  has_one :facebook_identity, -> { where provider: 'facebook' }, class_name: 'Identity'
+  has_one :twitter_identity, -> { where provider: 'twitter' }, class_name: 'Identity'
+  has_one :linkedin_identity, -> { where provider: 'linkedin' }, class_name: 'Identity'
+  has_one :googleplus_identity, -> { where provider: 'googleplus' }, class_name: 'Identity'
 end
