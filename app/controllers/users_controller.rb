1f0112f89668567e1490e6a33bbcdc165590867f
Zachary Foster <zach@murfie.com>
2013-09-25 10:25:00 -0500

    create list of emails, add ability to choose one of them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @identities = @user.identities
+    @emails = []
+    @identities.each do |i|
+      if i.info['email']
+        @emails << i.info['email']
+      end
+    end
   end
 
   def update
