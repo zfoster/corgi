7dd5d91ec7b5a9996392259246c3e2d5349a1757
Tiffany Green <tiffanydgreen@gmail.com>
2013-09-25 11:11:21 -0500

    remove unnneeded helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,3 @@
 module UsersHelper
-  def obtain_email
-    unless @user.email.present?
-      form_for @user do |f|
-        f.label :email, "Please provide an email address"
-        f.text_field :email, {required: 'required', placeholder: 'you@domain.com'}
-        f.submit 'Save Email'
-      end
-    end
-  end
+
 end
