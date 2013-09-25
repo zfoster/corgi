dba9991d8e21623458c61e90775283e318b16ea5
Zachary Foster <zach@murfie.com>
2013-09-25 09:44:58 -0500

    email round 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @identities = @user.identities
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(email: params[:user][:email])
+    redirect_to @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Corgi::Application.routes.draw do
   get '/support' => 'content#support', :as => :support
   root "home#index"
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
 
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/logout', to: 'sessions#destroy', via: [:get, :destroy]
