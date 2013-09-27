eedef592f111157bb6bb7e740d6bf31f4803d0d5
Zachary Foster <zach@murfie.com>
2013-09-27 11:16:30 -0500

    define it in heroku not rails

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Corgi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.secret_key_base = ENV['RAILS_SECRET']
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
