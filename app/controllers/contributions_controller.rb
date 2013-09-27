79f8a404fd6b2d8e3effd7eda6f5ce127d6d2949
Tiffany Green <tiffanydgreen@gmail.com>
2013-09-27 15:22:37 -0500

    Make params into Rails 4 way with strong

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,7 +1,12 @@
 class ContributionsController < ApplicationController
   def create
-    @contribution = Contribution.new
-    @contribution.update_attributes(note: params[:contribution][:note], email: params[:contribution][:email])
+    @contribution = Contribution.create(contribution_params)
     redirect_to support_content_index_path, notice: 'Thank you for contributing. Someone will be in touch with you shortly.'
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:note, :email)
   end
 end
