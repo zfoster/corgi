f4d1b75b208a2834ecbd580e80da70e78af13df4
Zachary Foster <zach@murfie.com>
2013-10-04 15:45:15 -0500

    update spec since no attributes are required one vent

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 
 describe EventsController do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Event. As you add validations to Event, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) { { "corgi_foreign_key" => "1" } }
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # EventsController. Be sure to keep this updated too.
+  let(:valid_attributes) { {} }
   let(:valid_session) { {} }
 
   describe "GET index" do
