5863cb34ef42653b52a33545bafb9ae15a5a17f9
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-04 14:40:33 -0500

    Change member to attendee. Closes #71

--- app/models/social_object.rb
+++ app/models/social_object.rb
@@ -1,6 +1,6 @@
 class SocialObject < ActiveRecord::Base
   has_many :admin, class_name: 'User'
-  has_many :members, class_name: 'User'
+  has_many :attendees, class_name: 'User'
   has_many :sharers, class_name: 'User'
   has_many :shares
 
