e8c2a30d70ee6c816f47e7d489ec2bffa3dc34ea
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-09 10:59:39 -0500

    Add new mailer methods

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,3 +1,14 @@
 class EventMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def event_created(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Thank you for creating an event!')
+  end
+
+  def attendees_updated(user)
+    @user = user
+    @url = ''
+    mail(to: @user.email, subject: 'Update on your event')
 end
