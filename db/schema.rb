f8af75e4739c94039c56634b57072c1861d2da83
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-09 10:48:38 -0500

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20131004202049) do
     t.datetime "updated_at"
   end
 
+  create_table "contributors", force: true do |t|
+    t.string   "email"
+    t.string   "contribution"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "events", force: true do |t|
     t.string   "title"
     t.text     "description"
