64681bc296f7e0a828675772a9a761fe6f05d0e1
Zach Foster <notzachfoster@gmail.com>
2013-10-11 12:01:14 -0500

    after registration create, send confirmation email

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,9 +3,14 @@ class Registration < ActiveRecord::Base
   belongs_to :user
 
   delegate :email, to: :user, prefix: true
+  after_create :confirm_attendance
 
   def cancel
     self.destroy
+  end
+
+  def confirm_attendance
+    RegistrationMailer.created(self).deliver
   end
 
 end
