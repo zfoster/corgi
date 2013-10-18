d0803a9ce08822caf6dd114814b3ffe9cccd79d3
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-18 15:57:07 -0500

    Change relationship of events and organizations to has_and_belongs_to_many

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,7 @@
 class Organization < ActiveRecord::Base
   has_many :events
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path("ORG.jpg")
+  # has_many :administrators, -> { distinct }, through: :events, source: :user
+  belongs_to :creator, class_name: "User"
   
 end
