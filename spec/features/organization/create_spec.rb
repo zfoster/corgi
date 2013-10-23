df641468363f02e14e43b746e3ec316ba7ff02f0
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-23 11:10:15 -0500

    WIP test for organization avatar image upload

--- spec/features/organization/create_spec.rb
+++ spec/features/organization/create_spec.rb
@@ -7,9 +7,16 @@ describe "An authenticated user creates an organization" do
     click_link 'Connect with LinkedIn'
     click_on 'ADD ORGANIZATION'
     fill_in 'Name', with: 'Madison Study Hall'
+    click_on 'Upload organization avatar'
+    post :change_avatar, :avatar => fixture_file_upload('/files/test.png', 'image/png')
 
     click_on 'Save'
     expect(page).to have_content('Madison Study Hall')
+    expect(page).to have_content('text.png')
+
+    after_create do |image, proxy|  #prevents memory overrun from lack of garbage collection
+      proxy.file.close
+    end
   end
   
 end
