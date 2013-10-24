21432fc05712b877e99d7a321cb6b3234cb79a6f
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-24 11:10:27 -0500

    Show default avatar if none specified

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ActiveRecord::Base
   has_many :events
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/ORG.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path("ORG.jpg")
   
 end
