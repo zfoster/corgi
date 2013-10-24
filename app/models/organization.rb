898925caebf5fc07acff91f1d13809632b733215
Tiffany Green <tiffanydgreen@gmail.com>
2013-10-24 10:55:24 -0500

    Allow uploading of avatar for organization and display on organization page. Closes #112

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,5 @@
 class Organization < ActiveRecord::Base
   has_many :events
-  attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/ORG.jpg", :path => 'dev/corgi/app/assets/images/paperclip/:filename'
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/ORG.jpg"
   
 end
