b904a1216f0cfac0cd63aa7765d4294c451e08e4
Steve Faulkner <southpolesteve@gmail.com>
2014-04-07 14:27:06 -0500

    Fix CSV output

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -24,7 +24,7 @@ class RegistrationsController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.csv { render csv: @event.registrations }
+      format.csv { render csv: @event.registrations.to_comma }
     end
   end
 
